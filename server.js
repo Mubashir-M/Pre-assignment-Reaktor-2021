86dec430f4f1a49946802f58d07173293bf997d8
mohamum1 <mubashir.mohamud@aalto.fi>
2021-07-22 16:50:49 +0300

    update

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(cors({
 
 if (process.env.NODE_ENV === "production"){
   app.use(express.static('build'));
-  app.get('*', (req,res) => {
+  /*app.get('*', (req,res) => {
     req.sendFile(path.resolve(__dirname,'build', 'index.html'));
-  })
+  })*/
 }
 
 app.get('/*', (req, res) => {
