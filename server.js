b41e7925ec8bf86a214201798b5c9228f8604e94
mohamum1 <mubashir.mohamud@aalto.fi>
2021-07-22 16:24:48 +0300

    update

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
-
+const port = process.env.PORT || 3000;
 app.use(cors({
   origin: '*'
 }));
 
-
-
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static('build'));
+  app.get('*', (req,res) => {
+    req.sendFile(path.resolve(__dirname,'build', 'index.html'));
+  })
+}
 
 app.get('/*', (req, res) => {
   let endpoint = process.env.API_BASE_URL
@@ -21,4 +26,4 @@ app.get('/*', (req, res) => {
       res.json(error)
     })
 })
-app.listen(process.env.PORT || 3000);
+app.listen(port);
